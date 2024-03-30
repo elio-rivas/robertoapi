55c524f5d24162dae3eb9db0cf75ccf037d92e37
elio-rivas <eliocarmen@hotmail.com>
2024-03-29 18:50:21 -0600

    updating endpoint subservices with Observable

--- src/modules/subservices/subservices.resolver.ts
+++ src/modules/subservices/subservices.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Int, Query, Resolver } from "@nestjs/graphql";
 import { SubservicesType } from "./subservices.type";
 import { SubservicesService } from "./subservices.service";
 import { CatalogLogger } from "../catalog/catalog.logger";
+import { catchError, map, Observable } from "rxjs";
 
 @Resolver(of => SubservicesType)
 export class SubservicesResolver{
@@ -11,19 +12,19 @@ export class SubservicesResolver{
   ) {}
 
   @Query(of => [SubservicesType])
-  async getSubServices(
+  getSubServices(
     @Args('id', { nullable: true, type: () => Int }) id: number | null,
     @Args('serviceId', { type: () => Int }) serviceId: number,
     @Args('lancode', { type: () => String }) lancode: string,
     @Args('ccode', { type: () => String }) ccode: string
-  ): Promise<SubservicesType[]>{
-    try {
-      const subservices = await this.service.getSubServices(id, serviceId, lancode, ccode);
-      return subservices.map(sser => ({ ...sser, id: +sser.id})); // Convert the id to a number and return the object with the id as a number
-    }catch (error){
-      this.logger.error('Error getting services in resolver', error.stack);
-      throw new Error('Failed to get services'); // You can customize the e
-    }
+  ): Observable<SubservicesType[]>{
+      return this.service.getSubServices(id, serviceId, lancode, ccode).pipe(
+        map(subservices => subservices.map(sser => ({ ...sser, id: +sser.id}))),
+        catchError(error => {
+          this.logger.error('Error getting SubServices in resolver', error.stack);
+          throw new Error('Failed to get SubServices'); // You can customize the error message here.
+        })
+      );
   }
 
 }
--- src/modules/subservices/subservices.service.ts
+++ src/modules/subservices/subservices.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Subservices } from "./subservices.entity";
 import { Repository } from "typeorm";
 import { CatalogLogger } from "../catalog/catalog.logger";
+import { catchError, from, Observable, throwError } from "rxjs";
 
 @Injectable()
 export class SubservicesService {
@@ -12,13 +13,14 @@ export class SubservicesService {
     private logger: CatalogLogger
   ) {}
 
-  async getSubServices(id: number, serviceId: number, lancode: string, ccode: string) {
-    try {
-      const services = await this.SubServicesRepository.query(`select * from operative.get_subservices($1,$2,$3,$4)`,[id, serviceId,lancode, ccode]);
-      return services || [];
-    }catch (error){
-      this.logger.error(`Error retrieving services translations for ID ${id}: ${error.message}`, error.stack);
-      throw new NotFoundException(`SubService translations for ID ${id} not found`);
-    }
+  getSubServices(id: number, serviceId: number, lancode: string, ccode: string): Observable<Subservices[]> {
+    return from (
+      this.SubServicesRepository.query(`select * from operative.get_subservices($1, $2, $3, $4)`, [id, serviceId, lancode, ccode])
+    ).pipe(
+      catchError(error => {
+        this.logger.error(`Error retrieving SubServices translations for ID ${id}: ${error.message}`, error.stack);
+        return throwError(new NotFoundException(`SubService translations for ID ${id} not found`));
+      })
+    );
   }
 }
